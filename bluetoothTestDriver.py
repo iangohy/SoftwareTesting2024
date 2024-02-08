d961ba96c367f5dc9f4b85106356a98bbf5072da
jiahui <clash9529@gmail.com>
2024-02-08 15:51:16 +0800

    for experimentaling purposes

---
+++ bluetoothTestDriver.py
@@ -0,0 +1,15 @@
+from generalFuzzer import *
+
+def main():
+    host = "127.0.0.1"
+    port = 5683
+
+    fuzzer = GeneralFuzzer(host, port)
+    #while(1):
+    #    try:
+    fuzzer.fuzz_and_send_requests_bluetooth(num_requests=3, num_bytes=5)
+    #    except:
+    fuzzer.close_connection()
+
+if __name__ == "__main__":
+    main()
