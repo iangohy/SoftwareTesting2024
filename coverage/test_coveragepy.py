a214050e239cfafa30ca93521d50876658c7874c
Kat Yong Jie <katyongjie@yahoo.com.sg>
2024-03-09 17:06:42 +0800

    IsInteresting method introduced

---
+++ coverage/test_coveragepy.py
@@ -0,0 +1,43 @@
+from coverage import Coverage
+
+def test_function(x):
+    i = 0
+    y = 0
+    while i < 100:
+        if x[i] < 5:
+            y += 1
+        else:
+            y -= 1
+        i += 1
+    assert(y <= 100)
+
+input_list = [
+    # interesting; if*100
+    [0] * 100,
+    # interesting; different branch; else*100
+    [10] * 100,
+    # not interesting; since 50 and 100 belong in the same bucket; if*50, else*50
+    [0 if i < 50 else 10 for i in range(100)],
+    # not interesting; if*50, else*50
+    [10 if i < 50 else 0 for i in range(100)],
+    # interesting; if*25, else*75
+    [0 if i < 25 else 10 for i in range(100)],
+    # interesting; if*75, else*25
+    [10 if i < 25 else 0 for i in range(100)],
+    # interesting; if*6, else*94
+    [-1 if i < 5 else 100 for i in range(100)],
+]
+
+def main():
+    for index, i in enumerate(input_list):
+        print("\nInput {}".format(index))
+        cov = Coverage()
+        cov.start()
+        test_function(i)
+        cov.stop()
+
+        executed_lines = cov.json_report(outfile="report{}".format(index))
+        print(executed_lines)
+
+if __name__ == "__main__":
+    main()
---
+++ coverage/test_fuzzingbook.py
@@ -0,0 +1,51 @@
+import json, random
+from IsInteresting import is_interesting
+
+def test_function(x):
+    i = 0
+    y = 0
+    while i < 100:
+        if x[i] < 5:
+            y += 1
+        else:
+            y -= 1
+        i += 1
+    assert(y <= 100)
+
+input_list = [
+    # interesting; if*100
+    [0] * 100,
+    # interesting; different branch; else*100
+    [10] * 100,
+    # not interesting; since 50 and 100 belong in the same bucket; if*50, else*50
+    [0 if i < 50 else 10 for i in range(100)],
+    # not interesting; if*50, else*50
+    [10 if i < 50 else 0 for i in range(100)],
+    # interesting; if*25, else*75
+    [0 if i < 25 else 10 for i in range(100)],
+    # interesting; if*75, else*25
+    [10 if i < 25 else 0 for i in range(100)],
+    # interesting; if*5, else*95
+    [-1 if i < 5 else 100 for i in range(100)],
+]
+
+random_input_list = [
+    [random.randint(0, 100) for _ in range(100)],
+    [random.randint(0, 100) for _ in range(100)],
+    [random.randint(0, 100) for _ in range(100)],
+    [random.randint(0, 100) for _ in range(100)],
+    [random.randint(0, 100) for _ in range(100)],
+]
+
+def main():
+    global_map = {}
+    buckets = [1, 2, 3, 4, 8, 16, 32, 128] # tunable
+
+    for index, i in enumerate(input_list):
+        print("\nInput {}".format(index))
+        is_interesting_result = is_interesting(test_function, i, global_map, buckets)
+        # print(json.dumps(global_map, indent=4, sort_keys=True))
+        print(is_interesting_result)
+
+if __name__ == "__main__":
+    main()
