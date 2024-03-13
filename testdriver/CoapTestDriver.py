5d2bf67db6ab62f6d1b47481ede08aedfe673fd0
jiahui <clash9529@gmail.com>
2024-03-13 20:12:07 +0800

    remove pycache and edit driver

--- testdriver/CoapTestDriver.py
+++ testdriver/CoapTestDriver.py
@@ -9,31 +9,34 @@ class CoapTestDriver:
     def __init__(self):
         self.server_url = "coap://127.0.0.1:5683"
         self.endpoints = ['/basic', '/storage', '/child', '/separate', '/etag', '/', '/big', '/encoding', '/advancedSeparate', '/void', '/advanced', '/long', '/xml']
-        
-
-    def generate_random_structure(self):
-        # generate_random_structure
-        pass
-
-    def generate_random_input(self, structure):
-        # generate_random_input based on structure
-        # should return test case as a whole in ASCII
-        pass
     
-    def interpret(self, input_data):
-        # interpret ASCII to fit to request
-        
-        return "", ""
+    # oracle to pass in the amount of test and list of inputs
+    async def run_fuzz_tests(self, num_tests, list_of_inputs):
+        for i in range(num_tests):
+            """
+            When oracle passed in list of inputs 
+             - response = await self.send_request(list_of_inputs[i])
+             
+            Note: Are we fuzzing endpoints?
+            """
+            
+            # dummy
+            response = await self.send_request(list_of_inputs)
+            logging.debug(response)
+            self.analyze_results(response)
     
     async def send_request(self, input_data):
-        # Send a request to the server with the provided input data
-        # choose from self.endpoints
-        endpoint = "/your_endpoint" 
-        url = f"{self.server_url}{endpoint}"
-        # call interpret function to get readable inputs?
-        input_data, headers = self.interpret(input_data)
+        """
+        Call interpret function to get readable inputs
+         - input_data, headers = self.interpret(input_data)
         
-        # example
+        Note: Fuzzing Message code, uri, message type, payload, token, message id, options, version?
+         - request = Message(code=input_data["code"], uri=input_data["url"], payload=input_data["payload"], mtype=input_data["mtype"], token=input_data["token"])
+        """
+        
+        url = f"{self.server_url}{self.endpoints[0]}"
+        
+        # dummy
         request = Message(code=Code.GET, uri=url, payload=input_data, mtype=0, token=bytes("token",'UTF-8'))
         logging.debug(request.token)
         context = await Context.create_client_context()
@@ -44,16 +47,16 @@ class CoapTestDriver:
     def analyze_results(self, response):
         # Analyze the results of the server response
         pass
+    
+    def interpret(self, input_data):
+        # interpret ASCII to fit to request
+        
+        return 0
 
-    async def run_fuzz_tests(self, num_tests):
-        for _ in range(num_tests):
-            structure = self.generate_random_structure()
-            input_data = self.generate_random_input(structure)
-            response = await self.send_request(input_data)
-            logging.debug(response)
-            self.analyze_results(response)
+    
         
     
 # Usage
-driver = CoapTestDriver()
-asyncio.run(driver.run_fuzz_tests(2))
+if __name__ == "__main__":
+    driver = CoapTestDriver()
+    asyncio.run(driver.run_fuzz_tests(2, []))
--- testdriver/DjangoTestDriver.py
+++ testdriver/DjangoTestDriver.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+import json
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -9,34 +10,44 @@ class DjangoTestDriver:
         self.endpoints = ["api/product/", "datatb/product/add/", "datatb/product/edit/",
                 "datatb/product/delete/", "datatb/product/export/", "accounts/register/", "accounts/login/"]
 
-    def generate_random_structure(self):
-        # generate_random_structure
-        pass
-
-    def generate_random_input(self, structure):
-        # generate_random_input based on structure
-        # should return test case as a whole in ASCII
-        pass
+    # oracle to pass in the amount of test and list of inputs
+    def run_fuzz_tests(self, num_tests, list_of_inputs):
+        for i in range(num_tests):
+            """
+            When oracle passed in list of inputs 
+             - response = await self.send_request(list_of_inputs[i])
+             
+            Note: Are we fuzzing endpoints or method?
+            """
+            
+            # dummy
+            response = self.send_request(list_of_inputs)
+            logging.debug(response)
+            self.analyze_results(response)
     
-    def interpret(self, input_data):
-        # interpret ASCII to fit to request
+    def send_request(self, input_data):
+        """
+        Call interpret function to get readable inputs
+         - input_data, headers = self.interpret(input_data)
         
-        return "", ""
-    
-    def send_request(self, input_data, method):
-        # Send a request to the server with the provided input data
-        # choose from self.endpoints
-        endpoint = self.endpoints[0] 
-        url = f"{self.server_url}{endpoint}"
-        # call interpret function to get readable inputs?
-        input_data, headers = self.interpret(input_data)
+        Note: Fuzzing Message code, uri, message type, payload, token, message id, options, version?
+         - request = Message(code=input_data["code"], uri=input_data["url"], payload=input_data["payload"], mtype=input_data["mtype"], token=input_data["token"])
+        """
         
-        # example input
+        url = f"{self.server_url}{self.endpoints[1]}"
+        
+        ### example input
         input_data = {
             'name': "hello",
             'info': "bye",
             'price': 1
         }
+        
+        input_data = json.dumps(input_data)
+        method = "get"
+        headers = {}
+        ################
+        
         if method == "post":
             response = requests.post(url, data=input_data, headers=headers)
         elif method == "get":
@@ -52,14 +63,15 @@ class DjangoTestDriver:
     def analyze_results(self, response):
         # Analyze the results of the server response
         pass
-
-    def run_fuzz_tests(self, num_tests):
-        for _ in range(num_tests):
-            structure = self.generate_random_structure()
-            input_data = self.generate_random_input(structure)
-            response = self.send_request(input_data, "get")
-            self.analyze_results(response)
+    
+    def interpret(self, input_data):
+        # interpret ASCII to fit to request
         
+        return 0
+
+    
+   
 # Usage
-driver = DjangoTestDriver()
-driver.run_fuzz_tests(2)
+if __name__ == "__main__":     
+    driver = DjangoTestDriver()
+    driver.run_fuzz_tests(2, [])
