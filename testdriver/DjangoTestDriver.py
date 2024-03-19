9976dff15fbc4569ff4ec9d4b3fc95ef8efbc0d3
Kat Yong Jie <katyongjie@yahoo.com.sg>
2024-03-19 17:05:38 +0800

    coverage testdriver integration init

--- testdriver/DjangoTestDriver.py
+++ testdriver/DjangoTestDriver.py
@@ -1,8 +1,16 @@
 import requests
 import logging
 import json
+import os
 
 logging.basicConfig(level=logging.DEBUG)
+
+# Configuration
+DJANGO_DIRECTORY = '../../DjangoWebApplication/'
+TEST_TEMPLATE_FILE = 'template_test.py'
+TEST_OUTPUT_FILE = 'tests.py'
+COVERAGE_JSON_FILE = 'output.json'
+MISSING_BRANCHES_FILE = 'missing_branches.json'
 
 class DjangoTestDriver:
     def __init__(self):
@@ -11,7 +19,7 @@ class DjangoTestDriver:
                 "datatb/product/delete/", "datatb/product/export/", "accounts/register/", "accounts/login/"]
 
     # oracle to pass in the amount of test and list of inputs
-    def run_test(self, list_of_inputs):
+    def run_test(self, list_of_inputs=[]):
         """
         When oracle passed in list of inputs 
             - response = await self.send_request(list_of_inputs[i])
@@ -20,10 +28,11 @@ class DjangoTestDriver:
         """
         
         # dummy
-        response = self.send_request(list_of_inputs)
-        logging.debug(response)
-        self.analyze_results(response)
-            
+        # response = self.send_request(list_of_inputs)
+        # logging.debug(response)
+        # self.analyze_results(response)
+
+        self.send_request_2(endpoint=self.endpoints[1], coverage=False)
     
     def send_request(self, input_data):
         """
@@ -59,6 +68,53 @@ class DjangoTestDriver:
         
         logging.debug(response.__dict__)
         return response
+    
+    def send_request_2(self, 
+            endpoint,
+            input_data={'name': "hello",'info': "bye",'price': str(1)}, 
+            method="post", # post | get | put | delete | patch
+            coverage=False
+        ):
+        """
+        Call interpret function to get readable inputs
+         - input_data, headers = self.interpret(input_data)
+        
+        Note: Fuzzing formData and header
+         - input_data["formData"] and input_data["headers"]
+        """
+        
+        text_to_replace = {
+            "ENDPOINT": '/'+endpoint,
+            "NAME": input_data['name'],
+            "INFO": input_data['info'],
+            "PRICE": input_data['price'],
+            "METHOD": method
+        }
+
+        # Reads the current template file
+        f = open(TEST_TEMPLATE_FILE, 'r')
+        data = f.read()
+        f.close()
+
+        # Replaces all keywords from text_to_replace
+        for var in text_to_replace.keys():
+            data = data.replace('|{}|'.format(var), text_to_replace[var])
+
+        # Writes new TestCase for Django
+        f = open(TEST_OUTPUT_FILE, 'w')
+        f.write(data)
+        f.close()
+    
+        if coverage:
+            # Runs the coverage command on the Django directory and generates a JSON report
+            os.system("coverage3 run --branch --omit='tests.py' {}manage.py test; coverage3 json --pretty-print -o {}".format(DJANGO_DIRECTORY, COVERAGE_JSON_FILE))
+
+            logging.info("Coverage run complete for {}".format(text_to_replace))
+        else:
+            # Runs the standard manage.py test command
+            os.system("python3 {}manage.py test".format(DJANGO_DIRECTORY))
+
+            logging.info("Run complete for {}".format(text_to_replace))
 
     def analyze_results(self, response):
         # Analyze the results of the server response
@@ -69,9 +125,7 @@ class DjangoTestDriver:
         
         return 0
 
-    
-   
 # Usage
 if __name__ == "__main__":     
     driver = DjangoTestDriver()
-    driver.run_fuzz_tests(2, [])
+    driver.run_test()
---
+++ testdriver/template_test.py
@@ -0,0 +1,36 @@
+import logging
+
+from django.test import TestCase, Client
+
+logging.basicConfig(level=logging.INFO)
+
+# coverage run --branch manage.py test [--keepdb]; coverage report > output.txt
+# coverage run --branch manage.py test; coverage json -o output.json
+
+class CoverageTests(TestCase):
+    def test_client(self):
+        c = Client()
+        endpoint = "|ENDPOINT|"
+
+        form_data = {
+            'name': "|NAME|",
+            'info': "|INFO|",
+            'price': "|PRICE|"
+        }
+        logging.info("Endpoint \t%s",endpoint)
+        logging.info("Input \t%s",form_data)
+
+        method = "|METHOD|"
+
+        if method == "post":
+            response = c.post(endpoint, form_data, content_type='application/json')
+        elif method == "delete":
+            response = c.delete(endpoint, form_data, content_type='application/json')
+        elif method == "put":
+            response = c.put(endpoint, form_data, content_type='application/json')
+        elif method == "patch":
+            response = c.patch(endpoint, form_data, content_type='application/json')
+        else:
+            response = c.get(endpoint, form_data, content_type='application/json')
+        
+        logging.info("Response \t%s",response.content.decode('utf-8'))
