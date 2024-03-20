90ac97632f5d083fe88964eaee86d2f0d2424744
Christopher Lye <christopher.zijian@gmail.com>
2024-03-20 17:23:51 +0800

    fix: main fuzzer

--- greybox_fuzzer/main_fuzzer.py
+++ greybox_fuzzer/main_fuzzer.py
@@ -1,4 +1,59 @@
 import logging
+from mutator import Mutator, Seed
+from typing import Any, List
+from oracle.oracle import Oracle
 
-log = logging.getLogger("greybox-logger")
+log = logging.getLogger(__name__)
 log.info("Hello, world")
+
+class MainFuzzer:
+    def __init__(self, init_seed, seedQ: List[Any], failureQ:List[Any], mode='ascii'):
+        self.original_seed = init_seed
+        self.seed = init_seed
+        self.seedQ = seedQ
+        self.failureQ = failureQ
+        self.oracle = Oracle("sudifuzz_config_example.ini")
+        self.mutator = Mutator(None, mode=mode) # set the mode here, seed here is for random
+            
+    def set_seed(self, seed):
+        """reset seeds of fuzzer to new seed"""
+        self.original_seed = seed
+        self.seed = seed
+        
+    def generate_mutations_ten(self):
+        """generate 10 consecutive mutations and append to self.seedQ"""
+        mutated_seed = self.seed
+        for _ in range(0,10):
+            mutated_seed = self.mutator.mutate(mutated_seed)
+            self.seedQ.append(mutated_seed)
+        return
+        
+    def getQueues(self):
+        """Return seedQ and failureQ"""
+        return self.seedQ, self.failureQ
+        
+    def fuzz(self):
+        """Fuzz one seed and send to oracle, return seedQ and failureQ"""
+        new_seed = self.mutator.mutate(self.seed)
+        self.seed = new_seed
+        self.seedQ.append(new_seed)
+        # TODO send to oracle, if failure received, append to failureQ
+        self.send_to_oracle()
+        return self.seedQ, self.failureQ
+        
+    # def fuzz_hundred(self):
+    #     for _ in range(0,100):
+    #         input = self.seedQ.pop() # pop last
+    #         # TODO send to oracle
+        
+    def send_to_oracle(self, seed):
+        # TODO write send to oracle
+        return
+
+if __name__ == '__main__':
+    seedQ = []
+    failureQ = []
+    seed = "This is a test seed."
+    mainfuzzer = MainFuzzer(seed, seedQ, failureQ)
+    mainfuzzer.fuzz()
+    
