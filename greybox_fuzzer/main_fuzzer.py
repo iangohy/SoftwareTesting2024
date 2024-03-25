8c903b43693d861850906d09f43e2af69dc97be4
Christopher Lye <christopher.zijian@gmail.com>
2024-03-25 17:27:06 +0800

    feat: assign energy v1

--- greybox_fuzzer/main_fuzzer.py
+++ greybox_fuzzer/main_fuzzer.py
@@ -4,6 +4,7 @@ from typing import Any, List
 import random
 from oracle.oracle import Oracle
 from smart_fuzzer.smartChunk import SmartChunk
+import math
 
 
 logger = logging.getLogger(__name__)
@@ -16,15 +17,38 @@ class MainFuzzer:
         self.max_fuzz_cycles = max_fuzz_cycles
         self.mutator = Mutator(None, mode=mode) # set the mode here, seed here is for random
         self.seed_idx = 0
+        self.first_flag = True # flag to check if first run of fuzzer
+        self.prev_energy = 100
+        self.energy = 100
+        # store parent seed validity
+        self.validity= 0
+
+        
+    def reset(self):
+        self.energy = 100
+        self.first_flag = True
             
     def set_seed(self, seed):
         """reset seeds of fuzzer to new seed"""
         self.original_seed = seed
         self.seed = seed
         
-    def assign_energy(self):
-        # Start with constant energy assignment
-        return 10
+    def degree_of_validity(self, seed, valid_inputs):
+        self.validity = valid_inputs / self.energy
+        return valid_inputs / self.energy
+        
+    def assign_energy(self, seed):
+        # if first run, default fuzz 100 times
+        if self.first_flag:
+            self.first_flag = False
+            self.energy = 100
+        else:
+        # return energy based on the previous oracle status
+        # possible ways is unique path, coverage, validity (syntactically)
+            previous_validity = self.validity
+            # divide by log to reduce the fuzzing of higher density regions? dk if it works or not
+            self.energy = 100 + (self.prev_energy * (previous_validity / math.log(self.energy)))
+        return self.energy
     
     def choose_next(self) -> SmartChunk:
         # Randomly choose an item from the seed q and pop out
@@ -53,18 +77,22 @@ class MainFuzzer:
                 return
             
             energy = self.assign_energy()
-
+            valid_inputs = 0
             for i in range(energy):
                 logger.info(f">> Energy cycle: {i+1}/{energy}")
                 mutated_chunk = next_input.mutate()
                 failure, isInteresting, info = self.send_to_oracle(mutated_chunk)
                 logger.info(f"Test result: failure {failure} | isInteresting {isInteresting} | info {info}")
+                # get validity info from oracle
+                valid_inputs += 1
                 if failure:
                     # Add to failure queue
                     self.failureQ.append(mutated_chunk)
                 elif isInteresting:
                     self.seedQ.append(mutated_chunk)
-
+            # record the validity of this previous seed
+            self.validity = self.degree_of_validity(next_input, valid_inputs)
+            self.prev_energy = energy
         return self.seedQ, self.failureQ 
 
     def send_to_oracle(self, chunk):
